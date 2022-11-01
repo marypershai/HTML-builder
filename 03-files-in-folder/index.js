f68824a5b31606e7472337160569da1591cda39c
Maryia Piarshai <mary.pershay@gmail.com>
2022-11-01 16:50:03 +0300

    feat: add 04-copy-dir solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,18 +4,18 @@ const path = require('path');
 const folderPath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
-        console.log("\nCurrent directory files:");
-        if (err)
-            console.log(err);
-        else {
-            files.forEach(file => {
-                if (!file.isDirectory()) {
-                    let fileName = file.name.toString().split('.');
-                    console.log(fileName[0] + " - " + fileName[1] + " - " + getFilesizeInKilobytes(file.name) + 'kb');
-                }
-            })
-        }
-    })
+    console.log("\nCurrent directory files:");
+    if (err)
+        console.log(err);
+    else {
+        files.forEach(file => {
+            if (!file.isDirectory()) {
+                let fileName = file.name.toString().split('.');
+                console.log(fileName[0] + " - " + fileName[1] + " - " + getFilesizeInKilobytes(file.name) + 'kb');
+            }
+        })
+    }
+})
 
 
 function getFilesizeInKilobytes(filename) {
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'files');
+const folderPathToCopy = path.join(__dirname, 'files-copy');
+
+fs.mkdir(folderPathToCopy, { recursive: true }, (err) => {
+    if (err) {
+        console.log("Error Found:", err);
+    } else {
+        deleteFiles();
+        fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+            if (err)
+                console.log(err);
+            else {
+                files.forEach(file => {
+                    copyFile(file.name);
+                })
+                console.log("Done!")
+            }
+        })
+    }
+})
+
+function copyFile(fileName) {
+    fs.copyFile(path.join(folderPath, fileName), path.join(folderPathToCopy, fileName), (err) => {
+        if (err) {
+            console.log("Error Found:", err);
+        }
+    });
+}
+
+function deleteFiles() {
+    fs.readdir(folderPathToCopy, (err, files) => {
+        if (err) {
+            throw err;
+        }
+
+        for (const file of files) {
+            fs.unlink(path.join(folderPathToCopy, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+}
+
 
