208098ad3da54f90fc66d26f0a8b9908e2d9c2a1
Maryia Piarshai <mary.pershay@gmail.com>
2022-11-01 15:06:32 +0300

    feat: add 03-files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+        console.log("\nCurrent directory files:");
+        if (err)
+            console.log(err);
+        else {
+            files.forEach(file => {
+                if (!file.isDirectory()) {
+                    let fileName = file.name.toString().split('.');
+                    console.log(fileName[0] + " - " + fileName[1] + " - " + getFilesizeInBytes(file.name) + 'kb');
+                }
+            })
+        }
+    })
+
+
+function getFilesizeInBytes(filename) {
+    const filePath = path.join(__dirname, 'secret-folder', filename);
+    let stats = fs.statSync(filePath);
+    let fileSizeInKilobytes = (stats.size / 1024).toFixed(3);
+    return fileSizeInKilobytes;
+}
